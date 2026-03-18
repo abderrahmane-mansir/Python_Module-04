c5d97678e7501c1253fc39e651b8053224b938b6
abderrahmane-mansir <mansir.0.abderrahmane@gmail.com>
2026-03-18 13:04:18 +0000

    ex0

---
+++ ex0/ft_ancient_text.py
@@ -0,0 +1,27 @@
+def vault_access(filename: str) -> None:
+    try:
+        with open(filename, "r") as file:
+            content = file.read()
+            print("Accessing Storage Vault: ", file.name)
+    except FileNotFoundError as e:
+        print(f"Error FileNotFoundError: {e}")
+    try:
+        print("Connection established...\n")
+        print("RECOVERED DATA:")
+        print(content)
+        print("\nData recovery complete. Storage unit disconnected.")
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+def main() -> None:
+    print("=== CYBER ARCHIVES - DATA RECOVERY SYSTEM ===\n")
+    filename = "ancient_fragment.txt"
+    vault_access(filename)
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
